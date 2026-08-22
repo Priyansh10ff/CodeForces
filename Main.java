6179ca8deac9d1525ab3ca4422bd1c52637e40f4
Priyansh Dugar <priyansh10jan@gmail.com>
2026-08-22 10:54:59 +0530

    Add solutions for problems 2207A, 2247A, and 4A

--- Main.java
+++ Main.java
@@ -1,17 +1,21 @@
-//2234B
 import java.util.*;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
         int t = sc.nextInt();
-        
+
         while(t > 0){
-            int n = sc.nextInt();
-            for(int i=0; i<n; i++){
-          
+            String s = sc.next();
+
+            if(s.length() < 11){
+                System.out.println(s);
             }
+            else{
+                int length = s.length()-2;
+                System.out.println(s.charAt(0) + "" + length + s.charAt(s.length()-1));
+            }
+            t--;
         }
-        t--;
     }
 }
